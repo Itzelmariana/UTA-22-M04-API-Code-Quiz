78b9b8395000bd75c0b8b31db14a6c26df5ccfb3
Myryitzy <104706728+Itzelmariana@users.noreply.github.com>
2022-07-01 01:05:55 -0500

    updated progress

--- README.md
+++ README.md
@@ -4,13 +4,14 @@ This challege requires creating an interactive coding quiz that includes multipl
 
 Acceptance criteria:
 
-- It must have a start button
-- Every question must have a timer and subtracts time when responded.
-- It must say if answer is correct or wrong
+- It must have a start button.
+- Every question must have a timer.
+- Timmer subtracts time when wrong answer is chosen.
+- It must say if answer is correct or incorrect.
 - Game is over when questions are answered or timer reaches 0
-- It can safe initials and score
+- It can store initials and score.
 
-Deployed project can be find at [link](https://itzelmariana.github.io/UTA-22-M03-Password-JS/).
+Deployed project can be find at [link](https://itzelmariana.github.io/UTA-22-M04-API-Code-Quiz).
 
 Mock-up
 ![The Password Generator application.](./docs/04-web-apis-homework-demo.gif)
--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,238 @@
-//Disapears Questions
-var questions = document.querySelector("#questions").children;
-// -->for loop to delete all questions
-for (var i = 0; i < questions.length; i++) {
-  var question = questions[i];
-
-  question.className = "d-none";
+if (!localStorage.getItem("highscores")) {
+  localStorage.setItem("highscores", JSON.stringify([]));
 }
+
+var score = {
+  total: 0,
+};
+
+function populateScores() {
+  var highscoresStored = document.querySelector("#result");
+  highscoresStored.innerHTML = "";
+
+  let scoreHistory = JSON.parse(localStorage.getItem("highscores"));
+
+  for (var i = 0; i < scoreHistory.length; i++) {
+    var si = scoreHistory[i];
+    var element = document.createElement("p");
+    element.textContent = si.initials + "-" + si.score;
+    highscoresStored.appendChild(element);
+  }
+}
+function clearHighscores(event) {
+  localStorage.setItem("highscores", JSON.stringify([]));
+  populateScores();
+}
+
+function hideHighscores() {
+  var scores = document.querySelector("#highscores");
+  scores.className = "d-none";
+}
+
+function showHome() {
+  hideQuestions();
+  hideHighscores();
+  hideTimer();
+
+  document.querySelectorAll("section,nav").forEach(function (node) {
+    node.classList.remove("d-none");
+  });
+}
+document.querySelector("#clear").addEventListener("click", clearHighscores);
+document.querySelector("#goback").addEventListener("click", showHome);
+document
+  .querySelector("#view-scores")
+  .addEventListener("click", showHighscores);
+
+//Disapears Questions
+function hideQuestions() {
+  var questions = document.querySelector("#questions").children;
+  // -->for loop to delete all questions
+  for (var i = 0; i < questions.length; i++) {
+    var question = questions[i];
+    question.className = "d-none";
+  }
+}
+hideQuestions();
+
 //Disapearing timer
-var timeRecord = document.querySelector("#time-display");
-timeRecord.className = "d-none";
+hideTimer();
+
+//Disapearing gameover
+var gameover = document.querySelector("#gameover");
+gameover.className = "d-none";
+
+//Disapearing scores
+var highscores = document.querySelector("#highscores");
+highscores.className = "d-none";
+
+// We use this variable to keep track of which
+// question is being answered.
+var currentQuestion = 0;
+
+// Timer stuff
+function getTimer() {
+  return parseInt(document.getElementById("timer").textContent);
+}
+
+function setTimer(val) {
+  document.querySelector("#timer").innerHTML = val;
+}
+
+function showTimer() {
+  var timeRecord = document.querySelector("#time-display");
+  timeRecord.className = "d-block";
+}
+
+function hideTimer() {
+  var timeRecord = document.querySelector("#time-display");
+  timeRecord.className = "d-none";
+}
 
 function countdown() {
-  var time = parseInt(document.getElementById("timer").textContent);
-  time = time - 1;
-  document.querySelector("#timer").innerHTML = time;
+  setTimer(Math.max(getTimer() - 1, 0));
+
+  if (getTimer() <= 0) {
+    endGame();
+  }
+}
+
+function setScore(totalScore) {
+  document.querySelector(".final-score").innerHTML = totalScore;
+}
+
+function endGame() {
+  clearInterval(interval);
+  setScore(score.total);
+  // show gameover
+  var gameover = document.querySelector("#gameover");
+  gameover.className = "d-block";
+
+  hideQuestions();
+}
+
+function handleAnswerClick(event) {
+  console.log(event);
+  var classes = event.target.classList;
+  var isCorrect = false;
+  for (var i = 0; i < classes.length; i++) {
+    var className = classes[i];
+    if ("correct" === className) {
+      isCorrect = true;
+    }
+  }
   var questions = document.querySelector("#questions").children;
+  var questionN = questions[currentQuestion];
+  if (isCorrect) {
+    score.total += 20;
+  } else {
+    setTimer(Math.max(getTimer() - 10, 0));
+  }
 
-  var question1 = questions[0];
+  if (currentQuestion + 1 < questions.length) {
+    var questionN = questions[currentQuestion];
+    var hr = document.createElement("hr");
+    var result = document.createElement("p");
+    result.className = "answer-result";
 
-  question1.className = "d-block";
-  timeRecord.className = "d-block";
+    if (isCorrect) {
+      result.textContent = "Correct";
+    } else {
+      result.textContent = "Incorrect";
+    }
 
-  if (time == 0) {
-    clearInterval(interval);
+    setTimeout(function () {
+      questionN.className = "d-none";
+      currentQuestion = currentQuestion + 1;
+      var questions = document.querySelector("#questions").children;
+      var question1 = questions[currentQuestion];
+      question1.className = "d-block";
+    }, 1000);
+    questionN.appendChild(hr);
+    questionN.appendChild(result);
+  } else {
+    endGame();
   }
 }
 
 function startCountdown() {
+  currentQuestion = 0;
   interval = setInterval(countdown, 1000);
+  score = { total: 0 };
+  setTimer(50);
+  var nodesWithFeedback = Array.from(document.querySelectorAll("p"))
+    .filter(
+      (el) => el.textContent === "Correct" || el.textContent === "Incorrect"
+    )
+    .concat(Array.from(document.querySelectorAll("hr")));
+
+  //+ Array.from(document.querySelectorAll("hr"));
+  for (var i = 0; i < nodesWithFeedback.length; i++) {
+    console.log(nodesWithFeedback);
+    nodesWithFeedback[i].remove();
+  }
   var sectionDelete = document.querySelector("section");
   sectionDelete.className = "d-none";
+  showTimer();
+  document.querySelector("#questions").children[0].className = "d-block";
 }
+
+function showHighscores() {
+  populateScores();
+  // show scores
+  endGame();
+  hideQuestions();
+
+  var highscoreShow = document.querySelector("#highscores");
+  highscoreShow.className = "d-block";
+
+  // hide everything else
+  var hideNav = document.querySelector("nav");
+  hideNav.className = "d-none";
+  var gameover = document.querySelector("#gameover");
+  gameover.className = "d-none";
+
+  document.querySelector("section").className = "d-none";
+}
+
+function storeInitials(event) {
+  event.preventDefault();
+  console.log(event);
+  var inputInitials = document.querySelector("#input-initials").value;
+
+  var storedScores = localStorage.getItem("highscores");
+  console.log("Stored scores", storedScores);
+
+  var parsedScores = JSON.parse(storedScores);
+  console.log("Parsed scores", parsedScores);
+
+  parsedScores.push({
+    initials: inputInitials,
+    score: score.total,
+  });
+  console.log("Parsed scores after adding", parsedScores);
+  localStorage.setItem("highscores", JSON.stringify(parsedScores));
+
+  showHighscores();
+}
+
+var btnInitials = document.querySelector("#submit-btn");
+btnInitials.addEventListener("click", storeInitials);
+
+var inputtext = document.querySelector("#input-initials");
+const enterKey = 13;
+inputtext.addEventListener("keyup", function (event) {
+  if (event.keyCode === enterKey) {
+    event.preventDefault();
+    document.getElementById("submit-btn").click();
+  }
+});
 
 var btnStartQuiz = document.querySelector("#start-quiz");
 btnStartQuiz.addEventListener("click", startCountdown);
+
+var btnAnswers = document.querySelectorAll("li");
+for (var i = 0; i < btnAnswers.length; i++) {
+  var btnAnswer = btnAnswers[i];
+  btnAnswer.addEventListener("click", handleAnswerClick);
+}
